1def84d772822e642057beb3b6fbcd97b815bdd9
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-22 12:51:35 +0200

    test: demo defaults

--- test.js
+++ test.js
@@ -11,4 +11,10 @@ eq(typeof tape, 'function', '`tape` a function')
 
 ok(true, 'a-ok', 'will wrap')
 
+// Falsy assertions are counted as flops
+const pass = tape(undefined, 2)
+
+pass(0, 'failing', 'will default')
+pass(1, 'passing')
+
 bill()
