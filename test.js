175e0a5300f127ee4e77b0dc201f5d3ebd4727b8
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-01-25 11:13:18 +0000

    test: cleanup

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
 const assert = require('assert')
 const { tape, exit } = require('./')
 
-const myTest = tape(assert.equal)
+const myTest = tape(assert.strict.strictEqual)
 const myThrows = tape(assert.throws)
 
 myTest
-  .describe('`exit` a function', 'exports as advertised')
+  .describe('`exit` a function')
   .test(typeof exit, 'function')
-  .describe('`tape` a function')
+  .describe('`tape` a function', 'exports as advertised')
   .test(typeof tape, 'function')
-  .test()
+  .test(2, 2)
 
 // Description-less (anon)
 myThrows.test(() => { throw Error })
 
 tape(assert)
-  .describe('a-ok', 'will wrap', 'ie. why not?')
+  .describe('a-ok', 'will wrap')
   .test(true)
   .tape(undefined)
   .describe('passing', 'done checking defaults')
