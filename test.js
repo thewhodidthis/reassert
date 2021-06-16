a5d82799988f4be743cf022cedccb1cd361aaf5b
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-16 23:21:42 +0100

    test: tweak import, fix for deno lint

--- test.js
+++ test.js
@@ -1,25 +1,27 @@
-import assert from 'assert'
-import { tape, exit } from './main.js'
+import assert from "assert"
+import { exit, tape } from "tapeling"
 
 const tapeStrictEqual = tape(assert.strict.strictEqual)
 
 tapeStrictEqual
-  .describe('`exit` a function')
-  .test(typeof exit, 'function')
-  .describe('`tape` a function', 'exports as advertised')
-  .test(typeof tape, 'function')
+  .describe("`exit` a function")
+  .test(typeof exit, "function")
+  .describe("`tape` a function", "exports as advertised")
+  .test(typeof tape, "function")
   .test(2, 2)
 
 const tapeThrows = tape(assert.throws)
 
 // Description-less (anon)
-tapeThrows.test(() => { throw Error })
+tapeThrows.test(() => {
+  throw Error
+})
 
 // Wrap whole of built in assertions module
 tape(assert)
-  .describe('a-ok', 'will wrap')
+  .describe("a-ok", "will wrap")
   .test(true)
   .tape(undefined)
-  .describe('passing', 'done checking defaults')
+  .describe("passing", "done checking defaults")
   .test()
   .exit()
